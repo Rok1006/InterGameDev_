7ed465a91bee10d8d637e27366c9b8f9575b3de4
Mitu <mitu@glowup.games>
2020-10-06 20:50:34 -0400

    started separating projects into namespaces

--- Assets/Scripts/FlatGame/Drag.cs
+++ Assets/Scripts/FlatGame/Drag.cs
@@ -2,46 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drag : MonoBehaviour
+
+namespace Flatgame
 {
-    private Vector2 screenPoint;
- private Vector2 offset;
 
- private bool selected;
+    public class Drag : MonoBehaviour
+    {
+        private Vector2 screenPoint;
+        private Vector2 offset;
 
- void Update(){
-     if(selected == true){
-         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         transform.position = new Vector2(cursorPos.x, cursorPos.y);
-     }
+        private bool selected;
 
-     if(Input.GetMouseButtonUp(0)){
-         selected = false;
-     }
- }
- 
- void OnMouseDown()
- {
+        void Update()
+        {
+            if (selected == true)
+            {
+                Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = new Vector2(cursorPos.x, cursorPos.y);
+            }
 
-    //  screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
- 
-    //  offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
- 
- }
- 
- void OnMouseDrag()
- {
+            if (Input.GetMouseButtonUp(0))
+            {
+                selected = false;
+            }
+        }
+
+        void OnMouseDown()
+        {
+
+            //  screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+            //  offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+        void OnMouseDrag()
+        {
 //      Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
- 
+
 //  Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) ;
 //  transform.position = curPosition;
- 
- }
 
- void OnMouseOver(){
-     Debug.Log("over");
-     if(Input.GetMouseButton(0)){
-         selected = true;
- }
-}
+        }
+
+        void OnMouseOver()
+        {
+            Debug.Log("over");
+            if (Input.GetMouseButton(0))
+            {
+                selected = true;
+            }
+        }
+    }
+
 }
