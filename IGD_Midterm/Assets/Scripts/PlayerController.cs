bf8fcb7d0ad0f5687e9488e75086b4f246f6c11a
ROK <hilary0310@gmail.com>
2020-10-20 09:55:08 -0400

    Add Scene 2 Cave

--- IGD_Midterm/Assets/Scripts/PlayerController.cs
+++ IGD_Midterm/Assets/Scripts/PlayerController.cs
@@ -19,27 +19,55 @@ public class PlayerController : MonoBehaviour
     private float jumpTimeCounter;
     public float jumpTime;
     private bool isJumping;
-    //private bool doubleJump;
     Animator animator;
-
+    public bool lampOut;
+    public int lamp;
+    public GameObject LampLight;
+    public GameObject LampLight2;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        lampOut = false;
+        lamp = 0;
+        LampLight.SetActive(false);
+        LampLight2.SetActive(false);
     }
 
     void FixedUpdate() //!
     {
-        //moveInput = Input.GetAxisRaw("Horizontal");
         Vector2 velocity = rb.velocity;
-        //velocity.x += movement.x * speed * Time.fixedDeltaTime;
-        //rb.velocity = velocity;
         rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);  
     }
 
     void Update()
     {
+        // if(lamp <1){
+        if(Input.GetKey(KeyCode.Z)){
+            lampOut = true;
+            lamp = 1; //change to 1
+            LampLight.SetActive(true);
+            LampLight2.SetActive(true);
+            
+        }
+       // }
+        // if(lamp >0){
+        if(Input.GetKey(KeyCode.X)){
+            lampOut = false;
+            lamp = 0;
+            LampLight.SetActive(false);
+            LampLight2.SetActive(false);
+            }
+       // }
+       if(lampOut){
+       if(Input.GetKeyDown(KeyCode.F)){
+           animator.SetTrigger("isAttackingwLamp");
+           animator.SetBool("isIDLEwLamp", false);
+       }
+       }
+        
+        //Movement Starts
         isGrounded = Physics2D.OverlapCircle(groundPos.position, checkRadius, whatIsGround);
 
         if (isGrounded == true && Input.GetKey(jumpKey))  //jump
@@ -55,13 +83,41 @@ public class PlayerController : MonoBehaviour
         
         if (isGrounded == true)
         {
+            if(!lampOut){
             animator.SetBool("isIDLE", true);
             animator.SetBool("isJumping", false);
+            //
+            animator.SetBool("isIDLEwLamp", false);
+            animator.SetBool("isJumpingwLamp", false);
+            animator.SetBool("isWalkingwLamp", false);
+            }
+            if(lampOut){
+            animator.SetBool("isIDLEwLamp", true);
+            animator.SetBool("isJumpingwLamp", false);
+            //
+            animator.SetBool("isIDLE", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isWalking", false);
+            }
         }
         else
         {
+            if(!lampOut){
             animator.SetBool("isIDLE", false);
             animator.SetBool("isJumping", true);
+            //
+             animator.SetBool("isIDLEwLamp", false);
+            animator.SetBool("isJumpingwLamp", false);
+            animator.SetBool("isWalkingwLamp", false);
+            }
+             if(lampOut){
+            animator.SetBool("isIDLEwLamp", false);
+            animator.SetBool("isJumpingwLamp", true);
+            //
+            animator.SetBool("isIDLE", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isWalking", false);
+            }
         }
 
         if (Input.GetKey(jumpKey) && isJumping == true)
@@ -92,40 +148,55 @@ public class PlayerController : MonoBehaviour
         {
             movement += Vector2.left;
         }
-        //if (Input.GetKey(jumpKey))
-        //{
-        //    animator.SetBool("isJumping", true);
-        //}
-        //else
-        //{
-        //    animator.SetBool("isJumping", false);
-        //}
 
         if (movement.x == 0)   //for animation just check the teacher sample
         {
+            if(!lampOut){
             animator.SetBool("isWalking", false);
+            //
+            animator.SetBool("isIDLEwLamp", false);
+            animator.SetBool("isJumpingwLamp", false);
+            animator.SetBool("isWalkingwLamp", false);
+            }
            // animator.SetBool("isIDLE", true);
+            if(lampOut){
+            animator.SetBool("isWalkingwLamp", false);
+            //
+            animator.SetBool("isIDLE", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isWalking", false);
+            }
         }
         else
         {
+             if(!lampOut){
             animator.SetBool("isWalking", true);
             animator.SetBool("isIDLE", false);
-
+            //
+            animator.SetBool("isIDLEwLamp", false);
+            animator.SetBool("isJumpingwLamp", false);
+            animator.SetBool("isWalkingwLamp", false);
+             }
+              if(lampOut){
+            animator.SetBool("isWalkingwLamp", true);
+            animator.SetBool("isIDLEwLamp", false);
+            //
+            animator.SetBool("isIDLE", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isWalking", false);
+             }
         }
 
         if (movement.x > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
-
-
-
         }
         else if (movement.x < 0)
         {
-            
             transform.eulerAngles = new Vector3(0, 180, 0);
-
         }
+        //movement Ends
+
     }
 }
 
